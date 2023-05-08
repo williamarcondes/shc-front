d8d7bc17b07546ac653fb7dd6ebe2745f39a1e09
williamarcondes <williamarcondes@gmail.com>
2023-05-08 04:18:19 -0300

    Feat: Refatora código

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,30 +2,24 @@ import Link from 'next/link';
 import { IconeLogo } from './Icones';
 
 export default function Header() {
+  const linkClass = 'text-gray-900 font-bold text-2xl mr-4 py-2 px-4 rounded-full bg-gray-100 hover:bg-gray-300';
+
+  const links = [
+    { href: '/', label: 'Clínicas' },
+    { href: '/quem-somos', label: 'Quem somos?' },
+    { href: '/feedback', label: 'Contato' },
+    { href: '/cadastro', label: 'Cadastro' },
+  ];
+
   return (
     <div className={'flex items-center justify-center py-4'}>
       {IconeLogo}
       <div className='ml-6'>
-        <Link href='/'>
-          <a className='text-gray-900 font-bold text-2xl mr-4 py-2 px-4 rounded-full bg-gray-100 hover:bg-gray-300'>
-            Clínicas
-          </a>
-        </Link>
-        <Link href='/quem-somos'>
-          <a className='text-gray-900 font-bold text-2xl mr-4 py-2 px-4 rounded-full bg-gray-100 hover:bg-gray-300'>
-            Quem somos?
-          </a>
-        </Link>
-        <Link href='/feedback'>
-          <a className='text-gray-900 font-bold text-2xl mr-4 py-2 px-4 rounded-full bg-gray-100 hover:bg-gray-300'>
-            Contato
-          </a>
-        </Link>
-        <Link href='/cadastro'>
-          <a className='text-gray-900 font-bold text-2xl mr-4 py-2 px-4 rounded-full bg-gray-100 hover:bg-gray-300'>
-            Cadastro
-          </a>
-        </Link>
+        {links.map((link) => (
+          <Link key={link.href} href={link.href}>
+            <a className={linkClass}>{link.label}</a>
+          </Link>
+        ))}
       </div>
     </div>
   );
